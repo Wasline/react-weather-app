0ed8fe3f6e7ed06b8c6ac9dd84f3f5129f3c39a9
wasline <sf.wasline@gmail.com>
2022-08-19 18:52:03 -0400

    Added the GitHub repo URL

--- src/App.js
+++ src/App.js
@@ -3,7 +3,13 @@ import './App.css';
 export default function App() {
   return (
     <div className="App">
-      Weather App
+      <h1>Weather App</h1>
+      <footer>
+        This project was coded by Wasline Saint fleur and is
+        <a href="https://github.com/Wasline/React" target="_blank">
+          open-sourced on GitHub
+        </a>
+      </footer>
     </div>
   );
 }
